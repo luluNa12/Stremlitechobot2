1c220be9a11239e08154faa13d0b799f6e483298
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-12-04 14:41:37 -0800

    Add chat history management to Echo Bot
    Initialize chat history and display previous messages.

--- echobot2.py
+++ echobot2.py
@@ -7,6 +7,19 @@ with st.chat_message("Lina"):
 with st.chat_message("Shoshani"):    
     st.write("Thank you ♡")
 
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
 # React to user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
