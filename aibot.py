fd4a4f7504f4f4dfd7fddc9117fe42b6ab03cbcf
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2026-01-03 07:07:32 -0800

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -66,6 +66,11 @@ st.title("AI chat")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Show the dashboard image once at the top
+with st.chat_message("assistant"):
+    st.image(“TheSavedImage.png", caption="CIT 144 – Demographics Data Visualization")
+
+
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
